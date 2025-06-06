5a2afb4a0670d4e0bc5a5419d2559128744239e9
jmdyz <1743446693@qq.com>
2025-06-06 16:41:03 +0800

    更新预训练脚本，调整GPU数量和WandB项目配置，修改模型名称及恢复检查点参数

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -3,19 +3,19 @@
 # THIS IS AN EXAMPLE SCRIPT. 
 # PLEASE CONFIGURE FOR YOUR SETUP.
 
-export WORLD_SIZE=1
-export WANDB_PROJECT="buildingsbenchv2.0.0"
-NUM_GPUS=1
+export BUILDINGS_BENCH=/data/lx/buildings-bench/v2.0.0/BuildingsBench
+export WANDB_ENTITY="jmdyz-"
+export WANDB_PROJECT="train"
+NUM_GPUS=4
 
-CUDA_VISIBLE_DEVICES=0 torchrun \
+torchrun \
     --nnodes=1 \
-    --nproc_per_node=1 \
+    --nproc_per_node=$NUM_GPUS \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
     scripts/pretrain.py \
-    --model TransformerWithGaussian-weather-M \
+    --model TransformerWithGaussian-L \
     --disable_slurm \
-    --num_workers 8 \
-    --train_idx_filename train_weekly_weather.idx \
-    --val_idx_filename val_weekly_weather.idx \
-    --disable_wandb
+    --num_workers 16 \
+    --resume_from_checkpoint TransformerWithGaussian-L_last.pt \
+    --wandb_run_id 2bt0e02i
