1d54f7e47ab54b26aea654e13d7c4a817dbccfd5
jmdyz <1743446693@qq.com>
2025-06-09 11:52:08 +0800

    更新预训练脚本，修改模型名称为TransformerWithGaussianAndMoEs-L，注释掉恢复检查点和WandB运行ID参数

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -8,14 +8,27 @@ export WANDB_ENTITY="jmdyz-"
 export WANDB_PROJECT="train"
 NUM_GPUS=4
 
+# torchrun \
+#     --nnodes=1 \
+#     --nproc_per_node=$NUM_GPUS \
+#     --rdzv-backend=c10d \
+#     --rdzv-endpoint=localhost:0 \
+#     scripts/pretrain.py \
+#     --model TransformerWithGaussian-L \
+#     --disable_slurm \
+#     --num_workers 16 \
+#     --resume_from_checkpoint TransformerWithGaussian-L_last.pt \
+#     --wandb_run_id 2bt0e02i
+
+
 torchrun \
     --nnodes=1 \
     --nproc_per_node=$NUM_GPUS \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
     scripts/pretrain.py \
-    --model TransformerWithGaussian-L \
+    --model TransformerWithGaussianAndMoEs-L \
     --disable_slurm \
-    --num_workers 16 \
-    --resume_from_checkpoint TransformerWithGaussian-L_last.pt \
-    --wandb_run_id 2bt0e02i
+    --num_workers 16
+    # --resume_from_checkpoint TransformerWithGaussian-L_last.pt \
+    # --wandb_run_id yz6jcske
